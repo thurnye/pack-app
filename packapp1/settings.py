faddb453425fabc9a5af3a0cc4f3384e6e8d0d03
Austin Im <imaustinim@gmail.com>
2021-04-02 19:34:30 -0400

    second

--- packapp1/settings.py
+++ packapp1/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
-environ.Env()
-environ.Env.read_env()
-import os
-my_key = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,9 +77,7 @@ WSGI_APPLICATION = 'packapp1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'packapp1',
-        'USER': 'postgres',
-        'PASSWORD': my_key,
+        'NAME': "Packd"
     }
 }
 
