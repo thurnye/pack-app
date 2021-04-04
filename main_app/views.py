6cc341d41de879811c3e387599f91d78ad9ea714
Austin Im <imaustinim@gmail.com>
2021-04-04 01:09:08 -0400

    sidebar

--- main_app/views.py
+++ main_app/views.py
@@ -5,32 +5,42 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'index.html')
 
+
 def search_city(request):
-  return render(request, 'search_city.html')
+    return render(request, 'search_city.html')
+
 
 def searched_city(request):
-  return render(request, 'search_filters.html') 
+    return render(request, 'search_filters.html')
+
 
 def searched_filters(request):
-  return render(request, 'results.html')
+    return render(request, 'results.html')
+
 
 def create(request):
-  return redirect('search/new/filters')
+    return redirect('search/new/filters')
+
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('/')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
+
+def test(request):
+    return render(request, "test.html")
