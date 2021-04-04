c2aba50002852a053306d0bf439a746a2b2d2d65
Logan <mlogan858@hotmail.com>
2021-04-04 11:36:02 -0400

    jquery/ajax upvoting/downvoting system build

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search_city, name='search_city'),
@@ -9,5 +10,7 @@ urlpatterns = [
     path('create', views.create, name="create"),
     path('accounts/signup', views.signup, name='signup'),
 
-    path("test", views.test, name="test")
+    path("test", views.test, name="test"),
+    path("post/ajax/up", views.upvote_system, name="upvote_system"),
+    path("post/ajax/down", views.downvote_system, name="downvote_system"),
 ]
