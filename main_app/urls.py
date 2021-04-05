e4033b3407d8d40e91546c1a17d2a1b1f506cbcc
Austin Im <imaustinim@gmail.com>
2021-04-05 00:14:53 -0400

    login

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,15 +1,17 @@
+from django.contrib.auth import views
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/signup', views.signup, name='signup'),
+
     path("search/", views.search, name="search"),
     # path('search/', views.search_city, name='search_city'),
     path('search/new', views.searched_city, name='searched_city'),
     path('search/new/filters', views.searched_filters, name='searched_filters'),
     path('create', views.create, name="create"),
-    path('accounts/signup', views.signup, name='signup'),
 
     path("test", views.test, name="test"),
     path("post/ajax/up", views.upvote_system, name="upvote_system"),
